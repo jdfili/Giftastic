36249bd9a731b63b5431d6bd6ad147f9918e4554
JD <jdfili14@gmail.com>
2018-12-06 12:35:14 -0800

    figure out play and pause

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
 
     var topics = ["basketball", "football", "soccer", "mma", "hockey"]
+    localStorage.getItem("topics");
 
     function renderButtons() {
+        console.log(localStorage.getItem("topics"));
         for (var i = 0; i < topics.length; i++) {
             var button = $("<button>");
             button.addClass("btn btn-primary topic");
@@ -15,24 +17,28 @@ $(document).ready(function () {
         $(".buttons").empty();
         event.preventDefault();
         topics.push($("#add-topic").val().trim());
+        localStorage.clear();
+        localStorage.setItem("topics", topics);
+        stored = true;
         renderButtons();
     })
 
     $(document).on("click", ".topic", function () {
+        $(".display").empty();
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + $(this).text() + "&api_key=Ig97Ln5sSfkudRTNHGujt1BpG3WxVTSi&limit=10"
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             var results = response.data;
-            $(".display").empty();
-            console.log(results)
-            console.log(results[0].images.fixed_height.url)
-            console.log(results[0].rating)
             for (var i = 0; i < results.length; i++) {
-                var item = $("<div>")
+                var item = $("<span>")
                 var giphy = $("<img>");
-                giphy.attr("src", results[i].images.fixed_height.url);
+                giphy.addClass("giphy");
+                giphy.attr("src", results[i].images.fixed_height_still.url);
+                giphy.attr("data-state", "still");
+                giphy.attr("data-still", results[i].images.fixed_height_still.url);
+                giphy.attr("data-animate", results[i].images.fixed_height.url);
                 item.append(giphy);
                 var rating = $("<p>");
                 rating.text("Rating: " + results[i].rating);
@@ -41,6 +47,21 @@ $(document).ready(function () {
             }
 
         })
+        $(document).on("click", ".giphy", function () {
+            state = $(this).attr("data-state");
+            console.log($(this).attr("data-still"))
+            console.log($(this).attr("data-animate"));
+            console.log($(this).attr("data-state"));
+            if (state == "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            }
+            else if (state == "animate") {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        })
+
     });
 
     renderButtons();
